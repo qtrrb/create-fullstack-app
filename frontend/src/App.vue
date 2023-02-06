fa5c3faeb19336309aaee2720a03db6fec8370dc
Quentin Torroba <torroba.q@gmail.com>
2023-02-06 10:28:51 +0100

    [Removed] Duplicate CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,38 +18,3 @@ onMounted(async () => {
   <NavBar />
   <RouterView />
 </template>
-
-<style scoped>
-.header-content {
-  padding-left: 2rem;
-  padding-right: 2rem;
-  max-width: 80rem;
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 3.5rem;
-}
-
-header h1 {
-  font-size: 1.25rem;
-  margin: 0;
-}
-
-header {
-  border-bottom: 1px #e5e7eb solid;
-  backdrop-filter: saturate(50%) blur(10px);
-  position: sticky;
-  top: 0;
-}
-
-.logo-with-text {
-  display: flex;
-  align-items: center;
-}
-
-.logo-with-text img {
-  margin-right: 0.5rem;
-}
-</style>
